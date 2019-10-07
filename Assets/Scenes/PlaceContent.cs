c7ac80890222f699db2f4b6f5cfdf2830dcaec9e
Matthew Hallberg <matthewhallberg@gmail.com>
2019-10-06 23:41:50 -0400

    added toggling of AR visualizers, added blocking clicks on IU.

--- Assets/Scenes/PlaceContent.cs
+++ Assets/Scenes/PlaceContent.cs
@@ -2,16 +2,18 @@
 using UnityEngine;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlaceContent : MonoBehaviour {
 
     public ARRaycastManager raycastManager;
-    public Camera arCamera;
+    public GraphicRaycaster raycaster;
 
     private void Update() {
 
-        if (Input.GetMouseButtonDown(0)) {
-
+        if (Input.GetMouseButtonDown(0) && !IsClickOverUI()) {
+        
             List<ARRaycastHit> hitPoints = new List<ARRaycastHit>();
             raycastManager.Raycast(Input.mousePosition, hitPoints, TrackableType.Planes);
 
@@ -21,5 +23,15 @@ public class PlaceContent : MonoBehaviour {
                 transform.position = pose.position;
             }
         }
+    }
+
+    bool IsClickOverUI() {
+        //dont place content if pointer is over ui element
+        PointerEventData data = new PointerEventData(EventSystem.current) {
+            position = Input.mousePosition
+        };
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(data, results);
+        return results.Count > 0;
     }
 }
---
+++ Assets/Scenes/ToggleAR.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class ToggleAR : MonoBehaviour {
+
+    public Toggle toggle;
+    public ARPlaneManager planeManager;
+    public ARPointCloudManager pointCloudManager;
+
+    bool activeState = true;
+
+    public void OnValueChanged(bool isOn) {
+        activeState = isOn;
+        VisualizePlanes();
+        VisualizePoints();
+    }
+
+    void VisualizePlanes() {
+        planeManager.enabled = activeState;
+        foreach (ARPlane plane in planeManager.trackables) {
+            plane.gameObject.SetActive(activeState);
+        }
+    }
+
+    void VisualizePoints() {
+        pointCloudManager.enabled = activeState;
+        foreach (ARPointCloud pointCLoud in pointCloudManager.trackables) {
+            pointCLoud.gameObject.SetActive(activeState);
+        }
+    }
+}
