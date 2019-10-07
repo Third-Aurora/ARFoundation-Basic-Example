a7f11fef6b6fa61d5f365b17a48d88aaf9ed6e54
Matthew Hallberg <matthewhallberg@gmail.com>
2019-10-06 21:37:03 -0400

    added example for placing objects.

---
+++ Assets/Scenes/PlaceContent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaceContent : MonoBehaviour {
+
+    public ARRaycastManager raycastManager;
+    public Camera arCamera;
+
+    private void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            List<ARRaycastHit> hitPoints = new List<ARRaycastHit>();
+            raycastManager.Raycast(Input.mousePosition, hitPoints, TrackableType.Planes);
+
+            if (hitPoints.Count > 0) {
+                Pose pose = hitPoints[0].pose;
+                transform.rotation = pose.rotation;
+                transform.position = pose.position;
+            }
+        }
+    }
+}
